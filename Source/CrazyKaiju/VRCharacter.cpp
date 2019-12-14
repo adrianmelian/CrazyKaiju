979bbb568f941d2d3256738bfc598cb587441d7c
amelian <adrianmelian123@gmail.com>
2019-12-13 17:46:01 -0800

    Initial Commit. Water, Controls, Destruction

---
+++ Source/CrazyKaiju/VRCharacter.cpp
@@ -0,0 +1,221 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//#include "Engine/World.h"
+#include "Engine/StaticMesh.h"
+#include "Curves/CurveFloat.h"
+#include "Camera/CameraComponent.h"
+#include "Camera/PlayerCameraManager.h"
+#include "Components/InputComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/PostProcessComponent.h"
+#include "XRMotionControllerBase.h"
+#include "MotionControllerComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+#include "NavigationSystem.h"
+#include "VRCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AVRCharacter::AVRCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Root for movement
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VR Root"));
+	VRRoot->SetupAttachment(GetRootComponent());
+
+	// VR Camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(VRRoot);
+
+	// Motion Controllers
+	MotionControllerLeft = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Motion Controller - Left"));
+	MotionControllerRight = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("Motion Controller - Right"));
+	MotionControllerLeft->SetupAttachment(GetRootComponent());
+	MotionControllerRight->SetupAttachment(GetRootComponent());
+	MotionControllerLeft->MotionSource = FXRMotionControllerBase::LeftHandSourceId;
+	MotionControllerRight->MotionSource = FXRMotionControllerBase::RightHandSourceId;
+	MotionControllerLeft->bDisplayDeviceModel = true;
+	MotionControllerRight->bDisplayDeviceModel = true;
+
+	// Destination Marker
+	DestinationMarker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Destination Mesh"));
+	DestinationMarker->SetupAttachment(VRRoot);
+
+	// Post Processing Component
+	PostProcessComponent = CreateDefaultSubobject<UPostProcessComponent>(TEXT("Post-Process Component"));
+	PostProcessComponent->SetupAttachment(VRRoot);
+}
+
+// Called when the game starts or when spawned
+void AVRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	DestinationMarker->SetVisibility(false);
+
+	if (VignetteMaterialBase == nullptr) return;
+	VignetteInstanceDynamic = UMaterialInstanceDynamic::Create(VignetteMaterialBase, this);
+	PostProcessComponent->AddOrUpdateBlendable(VignetteInstanceDynamic);
+}
+
+// Called every frame
+void AVRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CorrectCameraOffset();
+	UpdateDestinationMarker();
+	UpdateVignette();
+}
+
+void AVRCharacter::UpdateDestinationMarker()
+{
+	FVector OutLocation;
+	bool bHitDestination = GetTeleportDestination(OutLocation);
+
+	if (bHitDestination)
+	{
+		DestinationMarker->SetVisibility(true);
+		DestinationMarker->SetWorldLocation(OutLocation);
+	}
+	else
+	{
+		DestinationMarker->SetVisibility(false);
+	}
+}
+
+bool AVRCharacter::GetTeleportDestination(FVector& OutLocation)
+{
+	auto Start = MotionControllerLeft->GetComponentLocation();
+	FVector Look = MotionControllerLeft->GetForwardVector();
+	Look.RotateAngleAxis(30, MotionControllerLeft->GetRightVector());
+	auto End = Start + Look * MaxTeleportDistance;
+
+	FPredictProjectilePathParams PredictParams(TeleportProjectileRadius, Start, Look * MaxTeleportSpeed, TeleportSimulationTime, ECollisionChannel::ECC_Visibility, this);
+	PredictParams.DrawDebugType = EDrawDebugTrace::ForOneFrame;
+	PredictParams.bTraceComplex = true;
+	FPredictProjectilePathResult PredictResult;
+	bool bHit = UGameplayStatics::PredictProjectilePath(this, PredictParams, PredictResult);
+	if (!bHit) return false;
+
+	FNavLocation NavLocation;
+	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+	bool bHitNav = NavSystem->ProjectPointToNavigation(PredictResult.HitResult.Location, NavLocation, FVector(100, 100, 100));
+	if (!bHitNav) return false;
+
+	OutLocation = NavLocation.Location;
+
+	return true;
+}
+
+void AVRCharacter::BeginTeleport()
+{
+	StartFade(0, 1);
+
+	FTimerHandle Timer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AVRCharacter::Teleport, 0.5f, false);
+}
+
+void AVRCharacter::Teleport()
+{
+	float HalfHeight = GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+	FVector DestLocation = DestinationMarker->GetComponentLocation();
+	FVector Movement = FVector(DestLocation.X, DestLocation.Y, DestLocation.Z + HalfHeight);
+	SetActorLocation(Movement);
+	StartFade(1, 0);
+}
+
+void AVRCharacter::StartFade(float start, float end)
+{
+	// Fade
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController != nullptr)
+	{
+		PlayerController->PlayerCameraManager->StartCameraFade(start, end, 0.2f, FLinearColor::Black, false, true);
+	}
+}
+
+void AVRCharacter::UpdateVignette()
+{
+	if (VignetteVelocityRadius == nullptr) return;
+	float Speed = GetVelocity().Size();
+	float Radius = VignetteVelocityRadius->GetFloatValue(Speed);
+	VignetteInstanceDynamic->SetScalarParameterValue(FName("VignetteRadius"), Radius);
+
+	FVector2D MovementCenter = GetMovementCenter();
+	VignetteInstanceDynamic->SetVectorParameterValue(FName("VignetteCenter"), FLinearColor(MovementCenter.X, MovementCenter.Y, 0));
+}
+
+FVector2D AVRCharacter::GetMovementCenter()
+{
+	FVector MovementDirection = GetVelocity().GetSafeNormal();
+	if (MovementDirection.IsNearlyZero()) return FVector2D(0.5, 0.5);
+
+	FVector WorldStationaryLocation;
+	if (FVector::DotProduct(Camera->GetForwardVector(), MovementDirection) > 0)
+	{
+		WorldStationaryLocation = Camera->GetComponentLocation() + MovementDirection * 1000;
+	}
+	else
+	{
+		WorldStationaryLocation = Camera->GetComponentLocation() - MovementDirection * 1000;
+	}
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController == nullptr) return FVector2D(0.5, 0.5);
+
+	FVector2D ScreenStationaryLocation;
+	PlayerController->ProjectWorldLocationToScreen(WorldStationaryLocation, ScreenStationaryLocation);
+
+	int32 SizeX, SizeY;
+	PlayerController->GetViewportSize(SizeX, SizeY);
+	ScreenStationaryLocation.X /= SizeX;
+	ScreenStationaryLocation.Y /= SizeY;
+
+	return ScreenStationaryLocation;
+}
+
+void AVRCharacter::CorrectCameraOffset()
+{
+	FVector CameraOffset = Camera->GetComponentLocation() - GetActorLocation();
+	CameraOffset.Z = 0;
+	AddActorWorldOffset(CameraOffset);
+	VRRoot->AddWorldOffset(-CameraOffset);
+}
+
+// Called to bind functionality to input
+void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
+	PlayerInputComponent->BindAction(TEXT("Teleport"), IE_Released, this, &AVRCharacter::BeginTeleport);
+	PlayerInputComponent->BindAction(TEXT("RightTurn"), IE_Released, this, &AVRCharacter::RightTurn);
+	PlayerInputComponent->BindAction(TEXT("LeftTurn"), IE_Released, this, &AVRCharacter::LeftTurn);
+
+}
+
+void AVRCharacter::MoveForward(float Throttle)
+{
+	AddMovementInput(Throttle * Camera->GetForwardVector());
+}
+
+void AVRCharacter::MoveRight(float Throttle)
+{
+	AddMovementInput(Throttle * Camera->GetRightVector());
+}
+
+void AVRCharacter::RightTurn()
+{
+	VRRoot->AddWorldRotation(FRotator(0, 30, 0));
+}
+
+void AVRCharacter::LeftTurn()
+{
+	VRRoot->AddWorldRotation(FRotator(0, -30, 0));
+}
---
+++ Source/CrazyKaiju/VRCharacter.h
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "VRCharacter.generated.h"
+
+UCLASS()
+class CRAZYKAIJU_API AVRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveForward(float Throttle);
+	void MoveRight(float Throttle);
+
+	void RightTurn();
+	void LeftTurn();
+
+	void CorrectCameraOffset();
+	void UpdateDestinationMarker();
+
+	bool GetTeleportDestination(FVector& OutLocation);
+	void BeginTeleport();
+	void Teleport();
+
+	void StartFade(float start, float end);
+	void UpdateVignette();
+	FVector2D GetMovementCenter();
+
+
+	UPROPERTY(EditAnywhere)
+		class UMaterialInterface* VignetteMaterialBase = nullptr;
+
+	UPROPERTY(EditAnywhere)
+		class UCurveFloat* VignetteVelocityRadius = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		float TeleportTime = 3.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup") // TODO REMOVE
+		float MaxTeleportDistance = 500;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup") // TODO REMOVE
+		float TeleportSimulationTime = 2;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		float MaxTeleportSpeed = 750;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		float TeleportProjectileRadius = 10;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		int32 TurnStep = 15;
+
+	UPROPERTY(VisibleAnywhere)
+		class UCameraComponent* Camera = nullptr;
+
+	UPROPERTY(VisibleAnywhere)
+		class UMotionControllerComponent* MotionControllerLeft;
+
+	UPROPERTY(VisibleAnywhere)
+		class UMotionControllerComponent* MotionControllerRight;
+
+	UPROPERTY(VisibleAnywhere)
+		class USceneComponent* VRRoot = nullptr;
+
+	UPROPERTY(VisibleAnywhere) // Visible anywhere because you dont want to edit the pointer (*)
+		class UStaticMeshComponent* DestinationMarker = nullptr;
+
+	UPROPERTY()
+		class UPostProcessComponent* PostProcessComponent = nullptr;
+
+	UPROPERTY()
+		class UMaterialInstanceDynamic* VignetteInstanceDynamic = nullptr;
+
+
+};
